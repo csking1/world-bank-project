e28e20b2da4b33ea214d61d878d9adf996e372fe
Charity Susan King <csking1@mit.edu>
2016-05-17 16:12:35 -0500

    exploring features

---
+++ Features/gen.py
@@ -0,0 +1,28 @@
+import csv
+import pandas as pd
+import explore_clean as exp
+import numpy as np
+
+filename = "../Example/landing.csv"
+
+def data_summary(dataframe):
+ 
+    print("----------------Percentiles:-------------" "\n", np.round(dataframe.describe(percentiles = [.5]), 2).to_string(justify = "left"))
+    print("----------------Mean:--------------------" "\n", dataframe.mean().to_string(float_format = "{:.2f}".format))
+    print("----------------Median:------------------" "\n", dataframe.median().to_string(float_format = "{:.2f}".format))
+    print("----------------Standard Deviation:------" "\n", dataframe.std().to_string(float_format = "{:.2f}".format))
+    print("----------------Mode:--------------------" '\n', dataframe.mode().to_string(index = False))
+    print("----------------Correlation Matrix:------" "\n", dataframe.corr())
+    print("----------------Missing Values:----------" "\n", dataframe.isnull().sum().to_string())
+def read_data(filename):
+
+    df = pd.read_csv(filename)
+    return df
+def go(filename):
+    df = read_data(filename)
+    data_summary(df)
+    print (df.allegation_outcome)
+
+
+filename = "../Example/landing.csv"
+go(filename)
