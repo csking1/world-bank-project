77ed043df3c24645844b1fd46a63af7b305a8e3d
Charity Susan King <csking1@mit.edu>
2016-05-20 18:34:20 -0500

    oy

--- Features/gen.py
+++ Features/gen.py
@@ -5,7 +5,12 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+from sklearn.linear_model import LogisticRegression
 
+DROP = []
+BINARY = []
+CATEG = []
+LOG = []
 #variables to drop:
 #as_of_date
 #use feature selection as a preliminary
@@ -16,26 +21,41 @@ def read_data(filename):
     df = pd.read_csv(filename)
     return df
 
-def data_summary(dataframe):
-    pass
- 
-    # print("----------------Percentiles:-------------" "\n", np.round(dataframe.describe(percentiles = [.5]), 2).to_string(justify = "left"))
-    # print("----------------Mean:--------------------" "\n", dataframe.mean().to_string(float_format = "{:.2f}".format))
-    # print("----------------Median:------------------" "\n", dataframe.median().to_string(float_format = "{:.2f}".format))
-    # print("----------------Standard Deviation:------" "\n", dataframe.std().to_string(float_format = "{:.2f}".format))
-    # print("----------------Mode:--------------------" '\n', dataframe.mode().to_string(index = False))
-    # print("----------------Correlation Matrix:------" "\n", dataframe.corr())
-    # print("----------------Missing Values:----------" "\n", dataframe.isnull().sum().to_string())
+def binary_variable(dataframe, variable):
+    data[column] = data[column].apply(lambda x: 0 if \
+        column == variable else 1)
 
 
-def procurement_method(df):
-    pass
+def binary(dataframe, column):
+    dummies = pd.get_dummies(df[column])
+    df = df.join(dummies)
+    return df
 
+def binning_data(dataframe, variable, bins):
+
+    col = "bin " + str(variable)
+    dataframe[col] = pd.cut(dataframe[variable], bins=bins, \
+        include_lowest=True, labels=False)
+    return col
+
+def feature_generation(dataframe):
+    y = dataframe[Y_VAR]
+    y = np.ravel(y)
+    x = dataframe.drop(Y_VAR, 1)
+    model = LogisticRegression()
+    model = model.fit(x, y)
+    testing = model.score(x, y)
+    print ("accuracy score of {}".format(testing))
+
+    # return x, y
 
 
 def go(filename):
     df = read_data(filename)
-    data_summary(df)
+    feature_generation(df)
+
+
+ 
     # print (df.as_of_date)
     # print (df.borrower_contract_reference_number)
     # print (df.country)
@@ -43,8 +63,8 @@ def go(filename):
     # print (df.contract_description)
     # print (df.contract_signing_date)
     # print (df.major_sector)
-    print (df.procurement_category)
-    print (df.columns)
+    # print (df.procurement_category)
+    # print (df.columns)
 
 
 
@@ -53,3 +73,21 @@ def go(filename):
 if __name__ == '__main__':
     filename = "../Example/landing.csv"
 go(filename)
+
+
+
+
+
+# def feature_importance(x, dataframe):
+
+#     features = x
+#     clf = RandomForestClassifier(compute_importances=True)
+#     clf.fit(dataframe[features], dataframe[TARGET])
+#     importances = clf.feature_importances_
+#     sorted_idx = np.argsort(importances)
+#     padding = np.arange(len(features)) + 0.5
+#     pl.barh(padding, importances[sorted_idx], align='center')
+#     pl.yticks(padding, features[sorted_idx])
+#     pl.xlabel("Relative Importance")
+#     pl.title("Variable Importance")
+#     pl.show()
