bc929d019a41e8e6d37b65057efd831162239922
Emily Webber <egwebber@cs.uchicago.edu>
2016-05-30 10:05:26 -0500

    a few more basic commands for cleaning, probably do the whole dir in notebooks

--- Cleaning/summary.py
+++ Cleaning/summary.py
@@ -2,30 +2,41 @@ import csv
 import pandas as pd
 import sys
 
-
-
-def contracts(df):
+def summary(df):
 	'''
-	Takes a pandas data frame and generates summary statistics and charts
+	Takes a pandas data frame, generates summary statistics, and writes out a cleaned version of the csv
+	'''
+	print ("Mode:", df.mode())
+
+	# for i in range(df.shape[1]):
+	# 	print df.iloc(i)
+
+	# find nans and replace
+	# get correlations of columns with each other
+	# get
+	# write to csv
+
+def projects(df):
+	'''
+	Takes a data frame, generates summary statistics, and writes out a cleaned version of the csv
+	'''
+	print (df.shape)
+
+
+def investigations(df):
+	'''
+	Takes a data frame, generates summary statistics, and writes out a cleaned version of the csv
 	'''
 	print (df.shape)
 
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
 	take = pd.read_csv(sys.argv[1])
-	if "contracts" in sys.argv[1]:
-		contracts(take)
-	elif "projects" in sys.argv[1]:
-		projects(take)
-	elif "investigations" in sys.argv[1]:
-		investigations(take)
+	summary(take)
+	# if "contracts" in sys.argv[1]:
+	# 	contracts(take)
+	# elif "projects" in sys.argv[1]:
+	# 	projects(take)
+	# elif "investigations" in sys.argv[1]:
+	# 	investigations(take)
